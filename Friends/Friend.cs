a2f0a0bbc208be0b1320b2de6ee6d7ab30a267e7
max-student <max-student@maxtrain.com>
2021-02-22 11:40:16 -0500

    Initial Commit

--- Friends/Friend.cs
+++ Friends/Friend.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Friends {
-    class Friends {
+namespace FriendDictionary {
+    class Friend {
   
         public string Name { get; set; }
         public string Email { get; set; }
         public bool BFF { get; set; }
+        public long Phone { get; set; }
 
 
 
--- Friends/Program.cs
+++ Friends/Program.cs
@@ -1,26 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Friends {
+namespace FriendDictionary {
     class Program {
         static void Main(string[] args) {
 
 
-
-            var friends = new List<Friends>();
-
-            var Jess = new Friends { Name = "Jess", Email = "mynameisJess@yahoo.com", BFF = true };
-            friends.Add(Jess);
-
-            var Patty = new Friends { Name = "Patty", Email = "Patty@yahoo.com", BFF = true };
-            friends.Add(Patty);
+            var friend = new Dictionary<long, Friend>();
+           
 
 
-            foreach (var friend in friends) {
-                Console.WriteLine($"{friend.Name}");
+            var Jess = new Friend {Name = "Jess", Email = "mynameisJess@yahoo.com", BFF = true, Phone = 5134445555};
+            friend.Add(Jess.Phone,Jess);
+            
+
+            var Patty = new Friend {Name = "Patty", Email = "Patty@yahoo.com", BFF = true, Phone = 5133335555 };
+            friend.Add(Patty.Phone, Patty);
+            
+
+            var Robert  = new Friend {Name = "Robert", Email = "Robert@yahoo.com", BFF = true, Phone = 5137770000 };
+            friend.Add(Robert.Phone,Robert);
 
 
-            }
+            foreach (var phone in friend.Keys) {
+                var friends = friend[phone];
+                Console.WriteLine($"{friends.Phone} : {friends.Name}");
+
+
+           }
         }
     }
 }
